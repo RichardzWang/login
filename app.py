3cfc65ab897cc5e1d125b8090f65797cc16db441
Richard Wang <richardwang@Richards-MacBook-Pro-2.local>
2016-10-04 13:03:10 -0400

    Updated Login with Hash

--- app.py
+++ app.py
@@ -13,10 +13,22 @@ def login():
 
 @app.route("/authenticate/", methods = ['POST'])
 def auth():
-    if (authenticator.authenticator(request.form["username"],request.form["password"])):
-        return render_template('authenticate.html', message1 = "Success!", message2 = "Successfully logged in")
-    else:
-        return render_template('authenticate.html', message1 = "Failure", message2 = "Failed to log in")
+    #REGISTER
+    if (request.form["action"] == "REGISTER"):
+        if (authenticator.register(request.form["username"], request.form["password"])):
+            return render_template('login.html', message = "You have been registered")
+        else:
+            return render_template('login.html', message = "Your username has been taken")
+    #LOGIN
+    if (request.form["action"] == "LOGIN"):
+        number = (authenticator.auth(request.form["username"],request.form["password"]))
+    if number == -1:
+        return render_template('authenticate.html', message1 = "Access Granted", message2 = "Successfully logged in")
+    if number == -2:
+        return render_template('authenticate.html', message1 = "Access Denied", message2 = "Your password is incorrect")
+    if number == -3:
+        return render_template('authenticate.html', message1 = "Access Denied", message2 = "Your username does not exist")
+    return "error"
 
 if __name__ == '__main__':
     app.debug = 'TRUE'
--- utils/authenticator.py
+++ utils/authenticator.py
@@ -1,5 +1,42 @@
-username = 'user'
-password = 'pass'
+import csv, hashlib
 
-def authenticator(inusername,inpassword):
-    return (inusername == username and inpassword == inpassword)
+info = 'data/passwords.csv'
+
+def register(inusername, inpassword):
+    with open(info) as csvfile:
+        reader = csv.reader(csvfile)
+        unlocked = dict(reader)
+        for username in unlocked:
+            if username == inusername:
+                return False
+    with open(info, "w") as csvfile:
+        FieldNames = ['username','password']
+        writer = csv.DictWriter(csvfile, fieldnames = FieldNames)
+        hexedpass = hashlib.sha224(inpassword).hexdigest()
+        writer.writerow({'username' : inusername , 'password' : hexedpass})
+    return True
+
+def authuser(inusername):
+    with open(info) as csvfile:
+        reader = csv.reader(csvfile)
+        dictreader = dict(reader)
+        for username in dictreader:
+            if username == inusername:
+                return True
+    return False
+
+
+def auth(inusername,inpassword):
+    with open(info) as csvfile:
+        rawreader = csv.reader(csvfile)
+        reader = dict(rawreader)
+        for username in reader:
+            if username == inusername:
+                if (hashlib.sha224(inpassword).hexdigest() == reader[username]):
+                    return -1 #SUCCESSFUL
+                else:
+                    return -2 #PASSWORD INCORRECT
+            else:
+                return -3 #USERNAME INCORRECT
+
+        
