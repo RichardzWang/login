6fd2b526de54d417a6d58712634aca70242086d5
Richard Wang <richardwang@Richards-MacBook-Pro-2.local>
2016-10-10 20:09:19 -0400

    Added sessions

--- app.py
+++ app.py
@@ -2,10 +2,12 @@
 #SoftDev
 #Period9
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, url_for, redirect
 from utils import authenticator
 
 app = Flask(__name__)
+
+app.secret_key = "secret?"
 
 @app.route("/")
 def login():
@@ -15,20 +17,31 @@ def login():
 def auth():
     #REGISTER
     if (request.form["action"] == "REGISTER"):
-        if (authenticator.register(request.form["username"], request.form["password"])):
-            return render_template('login.html', message = "You have been registered")
-        else:
-            return render_template('login.html', message = "Your username has been taken")
+        return render_template('login.html', message = authenticator.register(request.form["username"], request.form["password"]))
+
     #LOGIN
     if (request.form["action"] == "LOGIN"):
         number = (authenticator.auth(request.form["username"],request.form["password"]))
     if number == -1:
-        return render_template('authenticate.html', message1 = "Access Granted", message2 = "Successfully logged in")
+        session['username'] = request.form['username']
+        return redirect(url_for('welcome'))
     if number == -2:
-        return render_template('authenticate.html', message1 = "Access Denied", message2 = "Your password is incorrect")
+        return render_template('login.html', message = "Your password is incorrect")
     if number == -3:
-        return render_template('authenticate.html', message1 = "Access Denied", message2 = "Your username does not exist")
+        return render_template('login.html', message = "Your username does not exist")
     return "error"
+
+@app.route("/welcome/")
+def welcome():
+    if 'username' in session:
+        return render_template('welcome.html', name = session['username'])
+    else:
+        return "Not logged in"
+
+@app.route("/logout/")
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.debug = 'TRUE'
--- utils/authenticator.py
+++ utils/authenticator.py
@@ -3,18 +3,23 @@ import csv, hashlib
 info = 'data/passwords.csv'
 
 def register(inusername, inpassword):
+    if len(inusername) < 3:
+        return "Your username must be longer than 2 characters" #USERNAME LENGTH LESS THAN 3
     with open(info) as csvfile:
         reader = csv.reader(csvfile)
         unlocked = dict(reader)
         for username in unlocked:
             if username == inusername:
-                return False
-    with open(info, "w") as csvfile:
+                return "Your username has been taken" #USERNAME TAKEN
+    if len(inpassword) < 3:
+        return "Your password must be longer than 2 characters" #PASSWORD LENGTH LESS THAN 3
+    with open(info, "a") as csvfile:
         FieldNames = ['username','password']
         writer = csv.DictWriter(csvfile, fieldnames = FieldNames)
         hexedpass = hashlib.sha224(inpassword).hexdigest()
         writer.writerow({'username' : inusername , 'password' : hexedpass})
-    return True
+    return "You have been registered" #REGISTER SUCCESSFUL
+
 
 def authuser(inusername):
     with open(info) as csvfile:
@@ -36,7 +41,6 @@ def auth(inusername,inpassword):
                     return -1 #SUCCESSFUL
                 else:
                     return -2 #PASSWORD INCORRECT
-            else:
-                return -3 #USERNAME INCORRECT
+    return -3 #USERNAME INCORRECT
 
         
